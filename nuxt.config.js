35a3d4e3ca4b0c720658ca9d2d2536d71d9de34a
Matthew Kirby <mkirby528@Matthews-MacBook-Air.local>
2022-07-08 17:28:06 -0400

    remove localhost hard coding

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   env: {
-    API_URL: process.env.API_URL
+    baseURL: process.env.baseURL
   },
   head: {
     titleTemplate: '%s - client',
