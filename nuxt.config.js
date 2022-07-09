7305c5e6980897a7c2678b8cbe53e56c91cbd4e1
Matthew Kirby <mkirby528@Matthews-MacBook-Air.local>
2022-07-09 01:43:50 -0400

    cors

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    proxy: true
+    proxy: true,
+    credentials: true
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
